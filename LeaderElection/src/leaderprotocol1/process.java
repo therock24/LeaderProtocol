8de604e749d2e7d523987db3633a59282b90bda3
vitor <vitor@hackster>
2018-01-06 03:32:12 +0000

    protocolo 1 a funcionar (faltam verificacoes de pointers)

--- LeaderElection/src/leaderprotocol1/process.java
+++ LeaderElection/src/leaderprotocol1/process.java
@@ -16,26 +16,31 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Collections;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 
 /**
  *
  * @author Vitor
  */
 public class process 
-{
-    private static final int alpha = 10; // min number of processes that do not crash
-    private static final int t_unit = 1; // time unit in miliseconds
-    private static final int eta = 100;
+{  
+    private static final int alpha = 1; // min number of processes that do not crash
+    private static final int t_unit = 100; // time unit in miliseconds
+    private static final int eta = 10*t_unit;
     public final int pid; // process id (java process id? )
-    private ArrayList<Integer> members; // contains all known pids
-    private Map<Integer,Timer> timer; // timer to check if link is timely
-    private Map<Integer,Integer> timeout; // timeout value increased when timer expires
-    private ArrayList<Integer> silent; // contains all processes that timer expired since last reset
-    private ArrayList<Integer> to_reset; // list of processes that will need to reset timer
-    private Map<Integer,Integer> susp_level; // level of suspicion that process j crashed 
-    private Map<Integer,ArrayList<Integer>> suspected_by; // list of processes that suspect process j
+    private CopyOnWriteArrayList<Integer> members; // contains all known pids
+    private ConcurrentMap<Integer,Timer> timer; // timer to check if link is timely
+    private ConcurrentMap<Integer,Integer> timeout; // timeout value increased when timer expires
+    private CopyOnWriteArrayList<Integer> silent; // contains all processes that timer expired since last reset
+    private CopyOnWriteArrayList<Integer> to_reset; // list of processes that will need to reset timer
+    private ConcurrentMap<Integer,Integer> susp_level; // level of suspicion that process j crashed 
+    private ConcurrentMap<Integer,CopyOnWriteArrayList<Integer>> suspected_by; // list of processes that suspect process j
     private int sn; // local seq number of message
-    private Map<Integer,message> state; // last message received by process k
+    private ConcurrentMap<Integer,message> state; // last message received by process k
     private CustomSocket csocket;
     private Thread RxThread;
     private Thread TimerThread;
@@ -48,17 +53,27 @@ public class process
     public process()
     {
             pid = Integer.parseInt(ManagementFactory.getRuntimeMXBean().getName().split("@")[0]);;
-            members = new ArrayList();
-            timer = new HashMap();
-            timeout = new HashMap();
-            silent = new ArrayList();
-            to_reset = new ArrayList();
-            susp_level = new HashMap();
-            suspected_by = new HashMap();
-            state = new HashMap();
-            state.put(this.pid,new message());
+            members = new CopyOnWriteArrayList();
+            timer = new ConcurrentHashMap();
+            timeout = new ConcurrentHashMap();
+            silent = new CopyOnWriteArrayList();
+            to_reset = new CopyOnWriteArrayList();
+            susp_level = new ConcurrentHashMap();
+            suspected_by = new ConcurrentHashMap();
+            state = new ConcurrentHashMap();
+            csocket = new CustomSocket(pid);
+            message aux = new message();
+            
             sn=0;
-            csocket = new CustomSocket();
+            aux.k = this.pid;
+            aux.snk = this.sn;
+            aux.silent = this.silent;
+            aux.susp_level = this.susp_level;
+            state.put(this.pid,aux);
+            members.add(this.pid);
+            susp_level.put(this.pid,0); 
+            
+            
             rxflag = true;
             txflag = true;
             //csocket.register(pid);
@@ -72,24 +87,30 @@ public class process
         //step 02
         for (int j : silent)
         {
-            ArrayList<Integer> list = suspected_by.get(j);
+            CopyOnWriteArrayList<Integer> list = suspected_by.get(j);
             if(list == null)
             {
-                list = new ArrayList();
+                list = new CopyOnWriteArrayList();
             }
             list.add(this.pid);
             suspected_by.put(j,list);
         }
         
         //step 03
+        CopyOnWriteArrayList alaux;
         for(int j : members)
         {
-            if (suspected_by.get(j).size() >= this.alpha)
+            alaux = suspected_by.get(j);
+            if(alaux != null)
             {
-                //step 04
-                susp_level.put(j,susp_level.get(j)+1);
+                if (alaux.size() >= this.alpha)
+                {
+                    //step 04
+                    susp_level.put(j,susp_level.get(j)+1);
+                }
+                suspected_by.put(j,new CopyOnWriteArrayList());
             }
-            suspected_by.put(j,new ArrayList());
+            
         }
         
         //step 05
@@ -97,7 +118,7 @@ public class process
         aux.k = this.pid;
         aux.snk = this.sn;
         aux.susp_level.putAll(this.susp_level);
-        aux.silent = (ArrayList<Integer>)this.silent.clone();
+        aux.silent = (CopyOnWriteArrayList<Integer>)this.silent.clone();
         state.put(this.pid,aux);
         
         //step 06
@@ -111,7 +132,7 @@ public class process
             t_aux.purge();
             this.timer.put(j,create_timer(j));
         }
-        this.to_reset = new ArrayList();
+        this.to_reset = new CopyOnWriteArrayList();
        
         //complete
     }
@@ -121,9 +142,12 @@ public class process
     {
         int min = 999999;
         int leader = 0;
+        System.out.println("silent = " + silent);
+        
         
         for(int j : members)
         {
+            System.out.println("susp_level " + j + " = " + susp_level.get(j));
             if(susp_level.get(j) < min )
             {   
                 min = susp_level.get(j);
@@ -142,61 +166,73 @@ public class process
     {
         //step 08
         timeout.put(j,timeout.get(j)+1);
-        silent.add(j);
+        
+        if(!silent.contains(j))
+            silent.add(j);
     }
+    
     
     public int processMessage(message msg)
     {
 
-        //step 11
-        if(this.members.contains(msg.k))
-        {   
-            //step 09
-            if(msg.snk <= this.state.get(msg.k).snk)
-            {
-                 return 0;
+            //step 11
+            if(this.members.contains(msg.k))
+            {   
+                //step 09
+                if(msg.snk <= this.state.get(msg.k).snk)
+                {
+                     return 0;
+                }
+                this.state.put(msg.k,msg);
+
+                //step12
+                //stop timer
+                this.to_reset.add(msg.k);
+                if(this.silent.contains(msg.k))
+                {
+                    this.silent.remove((Object)msg.k);
+                } 
+                this.timer.get(msg.k).cancel();
+
             }
-            this.state.put(msg.k,msg);
-            
-            //step12
-            //stop timer
-            this.to_reset.add(msg.k);
-            if(this.silent.contains(msg.k))
+            else
             {
-                this.silent.remove((Object)msg.k);
-            } 
-            this.timer.get(msg.k).cancel();
-            
-        }
-        else
-        {
-            //step13
-            this.state.put(msg.k,msg);
-            
-            //step14/15/16
-            this.susp_level.put(msg.k,0);
-            this.suspected_by.put(msg.k,new ArrayList());
-            this.timeout.put(msg.k,this.t_unit);
-            this.timer.put(msg.k,create_timer(msg.k));
-            this.members.add(msg.k);
-            this.to_reset.add(msg.k);
-                    
-        }
-        
-        //step18
-        for(int l : msg.susp_level.keySet())
-        {
-            this.susp_level.put(l,max(this.susp_level.get(l),msg.susp_level.get(l)));
-        }
-        
-        //step 19
-        for(int l : msg.silent )
-        {
-            ArrayList<Integer> aux = this.suspected_by.get(l);
-            aux.add(msg.k);
-            this.suspected_by.put(l,aux);
-        }
-        return 1;
+                //step13
+                this.state.put(msg.k,msg);
+
+                //step14/15/16
+                //System.out.println("putting 0 in" + msg.k + " ");
+                this.susp_level.put(msg.k,0);
+                this.suspected_by.put(msg.k,new CopyOnWriteArrayList());
+                this.timeout.put(msg.k,this.eta);
+                this.timer.put(msg.k,create_timer(msg.k));
+                this.members.add(msg.k);
+                this.to_reset.add(msg.k);
+
+            }
+
+            //System.out.println("keyset = " + msg.susp_level.keySet());
+            //step18
+            for(int l : msg.susp_level.keySet())
+            {
+                if(this.susp_level.get(l) != null)
+                {
+                    this.susp_level.put(l,max(this.susp_level.get(l),msg.susp_level.get(l)));
+                }
+                this.susp_level.put(l,max(0,msg.susp_level.get(l)));   
+            }
+
+            //step 19
+            for(int l : msg.silent )
+            {
+                CopyOnWriteArrayList<Integer> aux = this.suspected_by.get(l);
+                if(this.suspected_by.get(l) != null)
+                {
+                    aux.add(msg.k);
+                    this.suspected_by.put(l,aux); // alocar?
+                }  
+            }
+            return 1;
     }
 
     public int start()
@@ -213,7 +249,9 @@ public class process
                 if(msg != null)
                 {
                     processMessage(msg);
-                    System.out.println("recv count = " + sn);
+                    //System.out.println("recv count = " + sn);
+                    System.out.println("leader = " + leader() + " my.pid = " + pid + "");
+                    //System.out.println("timeout leader = " + timeout.get(leader()));
                 }
             }
         }
@@ -229,14 +267,14 @@ public class process
             {
                 try 
                 {
-                    Thread.sleep(10*eta);
+                    Thread.sleep(eta);
                 } 
                 catch (InterruptedException ex) 
                 {
                     Logger.getLogger(process.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 task1();
-                System.out.println("Seq num: " + sn);
+                //System.out.println("Seq num: " + sn);
             }
         }
     });
@@ -279,5 +317,10 @@ public class process
                 timerflag = false;
                 return 1;
             }
+    
+    public void outputStats()
+    {
+            csocket.Stats.saveStats();
+    }
 }
     
